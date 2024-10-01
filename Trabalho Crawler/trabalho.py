8780a9ce54fac49fabba36986d655e54689fb9d9
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-10-01 14:56:49 -0300

    Create trabalho.py

---
+++ Trabalho Crawler/trabalho.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def parsing(url):
+    response = requests.get(url)
+
+    site = BeautifulSoup(response.content, 'html.parser')
+
+    produtos_html = site.find_all('li', attrs={'class': 'item first'}) + site.find_all('li', attrs={'class': 'item'}) + site.find_all('li', attrs={'class': 'item last'})
+
+    produtos = []
+    check = []
+
+    for produto in produtos_html:
+        nome = produto.find('h2', attrs={'class': 'product-name'})
+        valor = produto.find('p', attrs={'class': 'valprincipal'})
+        
+        if nome and valor:
+            nome_produto = nome.get_text(strip=True)
+            valor_produto = valor.get_text(strip=True)
+
+            valor_produto = valor_produto.replace('R$', '').replace('Por', '').replace('.', '').replace(',', '.').strip()
+            valor_produto = float(valor_produto)  
+            
+            if nome_produto not in check:
+                check.append(nome_produto)
+                produtos.append((nome_produto, valor_produto))
+    
+    return produtos
