fe0221b4c8e798496ef7050c9846ad7c2910f84c
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2024-11-11 16:45:11 -0300

    Update abb.py

--- ABB/abb.py
+++ ABB/abb.py
@@ -1,112 +1,91 @@
 class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+    def __init__(self,valor,right=None,left=None):
+        self.valor = valor
+        self.right = right
+        self.left = left
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+    def __str__(self):
+        return str(self.valor)
 
-    def insert(self, key):
-        self.root = self._insert(self.root, key)
+def pre_ordem(arv):
+    if arv:
+        print(arv.valor)
+        pre_ordem(arv.left)
+        pre_ordem(arv.right)
 
-    def _insert(self, node, key):
-        if node is None:
-            return Node(key)
-        if key < node.key:
-            node.left = self._insert(node.left, key)
+def em_ordem(arv):
+    if arv:
+        em_ordem(arv.left)
+        print(arv.valor)
+        em_ordem(arv.right)
+
+def pos_ordem(arv):
+    if arv:
+        pos_ordem(arv.left)
+        pos_ordem(arv.right)
+        print(arv.valor)
+
+def insere(arv,v):
+    if arv is None:
+        return Node(v)
+    
+    if v < arv.valor:
+        arv.left = insere(arv.left,v)
+    else:
+        arv.right = insere(arv.right,v)
+    return arv
+
+def remove(arv, v):
+    if arv is None:
+        return None
+    
+    if v < arv.valor:
+        arv.left = remove(arv.left, v)
+    elif v > arv.valor:
+        arv.right = remove(arv.right, v)
+    else:
+        if arv.left is None and arv.right is None:
+            return None
+    
+        elif arv.left and arv.right is None:
+            return arv.left
+        
+        elif arv.left is None and arv.right:
+            return arv.right
+        
         else:
-            node.right = self._insert(node.right, key)
-        return node
-
-    def search(self, key):
-        return self._search(self.root, key)
-
-    def _search(self, node, key):
-        if node is None or node.key == key:
-            return node
-        if key < node.key:
-            return self._search(node.left, key)
-        return self._search(node.right, key)
-
-    def delete(self, key):
-        self.root = self._delete(self.root, key)
-
-    def _delete(self, node, key):
-        if node is None:
-            return node
-        if key < node.key:
-            node.left = self._delete(node.left, key)
-        elif key > node.key:
-            node.right = self._delete(node.right, key)
-        else:
-            if node.left is None:
-                return node.right
-            elif node.right is None:
-                return node.left
-                
-            temp = self._min_value_node(node.right)
-            node.key = temp.key
-            node.right = self._delete(node.right, temp.key)
-
-        return node
-
-    def _min_value_node(self, node):
-        current = node
-        while current.left is not None:
-            current = current.left
-        return current
-
-    def preorder(self):
-        self._preorder(self.root)
-        print()
-
-    def _preorder(self, node):
-        if node:
-            print(node.key, end=" ")
-            self._preorder(node.left)
-            self._preorder(node.right)
-
-    def inorder(self):
-        self._inorder(self.root)
-        print()
-
-    def _inorder(self, node):
-        if node:
-            self._inorder(node.left)
-            print(node.key, end=" ")
-            self._inorder(node.right)
-
-    def postorder(self):
-        self._postorder(self.root)
-        print()
-
-    def _postorder(self, node):
-        if node:
-            self._postorder(node.left)
-            self._postorder(node.right)
-            print(node.key, end=" ")
+            f = arv.left
+            while f.right is not None:
+                f = f.right
+            arv.valor = f.valor
+            arv.left = remove(arv.left, f.valor)
+    
+    return arv
 
 
-bst = BinarySearchTree()
+def busca(arv,v):
+    if arv is None:
+        return 
+    if v < arv.valor:
+        return busca(arv.left,v)
+    elif v > arv.valor:
+        return busca(arv.right,v)
+    return arv
 
-valores_para_inserir = [50, 30, 70, 20, 40, 60, 80]
-for valor in valores_para_inserir:
-    bst.insert(valor)
+arvore = None
+arvore = insere(arvore,4)
+arvore = insere(arvore,8)
+arvore = insere(arvore,5)
+arvore = insere(arvore,2)
+arvore = insere(arvore,1)
+arvore = remove(arvore,2)
+pre_ordem(arvore)
+print()
+em_ordem(arvore)
+print()
+pos_ordem(arvore)
 
-print("Percurso em pré-ordem:")
-bst.preorder() 
+print(" ")
 
-print("Percurso em in-ordem:")
-bst.inorder()  
-
-print("Percurso em pós-ordem:")
-bst.postorder()
-
-bst.delete(70) 
-
-
-print("Percurso em in-ordem após deleção do valor 70:")
-bst.inorder() 
-
+arvore1 = busca(arvore,1)
+print(arvore1)
